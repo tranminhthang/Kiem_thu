8b4001b514fb045ef7888a030e7f060bb2b3053d
tranminhthang <thangtm_55@vnu.edu.vn>
2013-09-29 20:21:02 +0700

    update

--- triangle/test.py
+++ triangle/test.py
@@ -2,53 +2,46 @@ import unittest
 import triangle
 import math
 
-class Testtamgiac(unittest.TestCase):
-    def test_testtamgiac_10(self):
+class test(unittest.TestCase):
+    def test_10(self):
         self.assertEquals(triangle.detect_triangle('b',5,6),"du lieu dau vao khong hop le")
-    def test_testtamgiac_11(self):
+    def test_11(self):
         self.assertEquals(triangle.detect_triangle(4,-5,6),"du lieu dau vao khong hop le")
-    def test_testtamgiac_12(self):
+    def test_12(self):
         self.assertEquals(triangle.detect_triangle(4,5,-6),"du lieu dau vao khong hop le")
         
 
-    def test_testtamgiac_13(self):
+    def test_13(self):
         self.assertEquals(triangle.detect_triangle(2**32 +40, 2**32-10, 2**32 -15),"du lieu dau vao khong hop le")
-    def test_testtamgiac_14(self):
+    def test_14(self):
         self.assertEquals(triangle.detect_triangle(2**32-10,2**32 +40, 2**32 -15),"du lieu dau vao khong hop le")
-    def test_testtamgiac_15(self):
+    def test_15(self):
         self.assertEquals(triangle.detect_triangle(2**32-10, 2**32 -15,2**32 +40),"du lieu dau vao khong hop le")
         
-    def test_testtamgiac_16(self):
+    def test_16(self):
         self.assertEquals(triangle.detect_triangle('b',5,6),"du lieu dau vao khong hop le")
-    def test_testtamgiac_17(self):
+    def test_17(self):
         self.assertEquals(triangle.detect_triangle(4,'d',6),"du lieu dau vao khong hop le")
-    def test_testtamgiac_18(self):
+    def test_18(self):
         self.assertEquals(triangle.detect_triangle(4,5,'e'),"du lieu dau vao khong hop le")
 
-    def test_testtamgiac_21(self):
+    def test_21(self):
         self.assertEquals(triangle.detect_triangle(4,4,9),"day khong phai la 1 tam giac")
-    def test_testtamgiac_22(self):
+    def test_22(self):
         self.assertEquals(triangle.detect_triangle(9,4,4),"day khong phai la 1 tam giac")
-    def test_testtamgiac_23(self):
+    def test_23(self):
         self.assertEquals(triangle.detect_triangle(9,4,4),"day khong phai la 1 tam giac")
 
         
-    def test_testtamgiac_7(self):
+    def test_5(self):
         self.assertEquals(triangle.detect_triangle(4,5,6),"tam giac thuong")
-    def test_testtamgiac_8(self):
+    def test_6(self):
         self.assertEquals(triangle.detect_triangle(4,4,4),"tam giac deu")
-    def test_testtamgiac_9(self):
+    def test_7(self):
         self.assertEquals(triangle.detect_triangle(4,4,5),"tam giac can")
-    def test_testtamgiac_5(self):
+    def test_8(self):
         self.assertEquals(triangle.detect_triangle(3,4,5),"tam giac vuong")
-    def test_testtamgiac_51(self):
-        self.assertEquals(triangle.detect_triangle(4,4,math.sqrt(32)),"tam giac vuong can")
-
-        
-
-        
-        
-  
-        
+    def test_9(self):
+        self.assertEquals(triangle.detect_triangle(4,4,math.sqrt(32)),"tam giac vuong can") 
 if __name__ == '__main__':
    unittest.main()
